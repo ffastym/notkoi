428d9f7abafeba6a199f17891d51fbfeffd73f9d
ffastym <yuriy.matviyuk22@gmail.com>
2024-04-10 19:47:34 +0300

    referrals

--- src/AppAuth.tsx
+++ src/AppAuth.tsx
@@ -3,7 +3,7 @@ import { useLoginQuery } from './App.operations.generated';
 
 export function AppAuth() {
   const searchParams = new URLSearchParams(window.location.search);
-  const referralCode = searchParams.get('start');
+  const referralCode = searchParams.get('tgWebAppStartParam');
   const { data, loading } = useLoginQuery({ fetchPolicy: 'cache-and-network', variables: { referralCode } });
 
   return data ? <App user={data.login} /> : loading ? <span>LOADING</span> : <span>Not authorised</span>;
--- src/sections/Profile.tsx
+++ src/sections/Profile.tsx
@@ -2,6 +2,7 @@ import { Overlay } from '../components/Overlay';
 import { DialogProps } from '../types';
 import { useReferFriendMutation, useUserQuery } from '../App.operations.generated';
 import { Button, FlexBoxCol } from '../components/styled/styled';
+import { useMemo } from 'react';
 
 export function Profile({ isVisible, hide }: DialogProps) {
   const { data } = useUserQuery({ fetchPolicy: 'cache-and-network' });
@@ -9,14 +10,32 @@ export function Profile({ isVisible, hide }: DialogProps) {
 
   const referFriend = async () => {
     await referRequest({ fetchPolicy: 'no-cache' });
-    window.Telegram.WebApp.close();
   };
+
+  const referralLink = useMemo(
+    () => `https://t.me/notkoi_bot/?start=${data?.user.referralCode}`,
+    [data?.user.referralCode],
+  );
+
+  // const btnClicked = useCallback(() => {
+  //   referFriend();
+  //   tg.sendData(JSON.stringify({ referralLink }));
+  // }, [referralLink, tg]);
+
+  // useEffect(() => {
+  //   tg.MainButton.setParams({ text: 'refer friend,', is_active: true, is_visible: true });
+  //   tg.MainButton.isVisible = true;
+  //   tg.MainButton.show();
+  //   tg.onEvent('mainButtonClicked', btnClicked);
+  //
+  //   return () => {
+  //     tg.offEvent('mainButtonClicked', btnClicked);
+  //   };
+  // }, [btnClicked, tg]);
 
   if (!data) {
     return null;
   }
-
-  const referralLink = `https://t.me/notkoi_bot/?start=${data?.user.referralCode}`;
 
   return (
     <Overlay visible={isVisible} title={'Profile'} onClose={hide}>
